5b243baa426a3858bfb260b8744b49f2f07a160a
S4cha <sachadso@gmail.com>
2019-12-16 10:04:43 +0100

    Updates usage in example project

--- KeyboardLayoutGuideExample/KeyboardLayoutGuideExample/ViewController.swift
+++ KeyboardLayoutGuideExample/KeyboardLayoutGuideExample/ViewController.swift
@@ -16,10 +16,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Opt out of safe area if needed.
-        // view.keyboardLayoutGuide.usesSafeArea = false
-        
         // Constrain your button to the keyboardLayoutGuide's top Anchor the way you would do natively :)
         button.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor).isActive = true
+        
+        // Opt out of safe area if needed.
+//         button.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuideNoSafeArea.topAnchor).isActive = true
     }
 }
